ef8eb127f2d1015a3fe41875ccf57b2eaa70cea4
Julia Locamuz <julialocamuz@MacBook-Pro-de-Julia.local>
2023-07-06 16:38:13 -0300

    querysample -- seleccion de cada boton de cada querysample

--- static/js/query.js
+++ static/js/query.js
@@ -1,6 +1,19 @@
 document.querySelectorAll('.query-button').forEach(button => {
-    button.addEventListener('click', (e) => {
-      const id = e.target.dataset.id;
-      console.log('Button ID:', id);
-    });
+    (function() {
+      const containerId = button.parentNode.id;
+      const buttons = document.querySelectorAll(`#${containerId} .query-button`);
+  
+      buttons.forEach(btn => {
+        btn.addEventListener('click', (e) => {
+          buttons.forEach(b => b.classList.remove('active')); // Remove "active" class from all buttons
+  
+          const id = e.target.dataset.id;
+          console.log('Button ID:', id);
+  
+          // Add "active" class to the clicked button
+          e.target.classList.add('active');
+        });
+      });
+    })();
   });
+  
