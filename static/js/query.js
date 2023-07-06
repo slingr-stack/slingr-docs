8f7a4bebf7211fb945f7378a9510f9bd71a07f01
Julia Locamuz <julialocamuz@MacBook-Pro-de-Julia.local>
2023-07-06 16:13:24 -0300

    querysample -- 1

--- static/js/query.js
+++ static/js/query.js
@@ -1,33 +1,6 @@
-function showTab(id, tabId) {
-    var tab = document.getElementById(tabId);
-    tab.classList.add('active');
-
-    var tabButton = document.querySelector('button[onclick="showTab(\'' + id + '\', \'' + tabId + '\')"]');
-    tabButton.classList.add('active');
-
-    var otherTabs = Array.from(tab.parentNode.children).filter(function(child) {
-        return child !== tab;
+document.querySelectorAll('.query-button').forEach(button => {
+    button.addEventListener('click', (e) => {
+      const id = e.target.dataset.id;
+      console.log('Button ID:', id);
     });
-
-    var otherTabButtons = Array.from(tabButton.parentNode.parentNode.children).filter(function(child) {
-        return child !== tabButton.parentNode;
-    });
-
-    otherTabs.forEach(function(otherTab) {
-        otherTab.classList.remove('active');
-    });
-
-    otherTabButtons.forEach(function(otherTabButton) {
-        otherTabButton.firstChild.classList.remove('active');
-    });
-}
-
-// Seleccionar el tab por defecto según la prioridad
-window.addEventListener('DOMContentLoaded', function() {
-    var defaultTab = document.querySelector('.default-tab');
-
-    if (defaultTab) {
-        var tabId = defaultTab.getAttribute('onclick').split('\'')[3];
-        showTab('{{ $id }}', tabId);
-    }
-});
+  });
